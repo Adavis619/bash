9a2000eead702db66e2d7ff3da6bed2ba62c84a7
Anthony Davis <adavis619@gmail.com>
2025-04-18 07:44:21 -0700

    Update list_reorder.sh

--- list_reorder.sh
+++ list_reorder.sh
@@ -4,11 +4,14 @@
 
 inputfile="$1"
 if [ ! -f "$inputfile" ]; then
-    echo "Error: File '$inputfile' not found."
+    echo "Usage: $0 <file>."
     exit 1
 fi
 
 # Create a temporary file for file processing.
-temp=$(mktemp)
+temp="$(mktemp)" 
 
-grep -v "#" $inputfile | sort -V | sed 's/^[[:space:]]*[0-9]\+[[:space:]]*//' |  awk '{print NR $0}' | tee -a $mktemp; grep "#" $inputfile | tee -a $mktemp
+# Backup original file
+
+grep -v "#" "$inputfile" | sort -V | sed 's/^[[:space:]]*[0-9]\+[[:space:]]*//' |  awk '{print NR $0}' | tee -a "$temp"; grep "#" "$inputfile" | tee -a "$temp"
+
