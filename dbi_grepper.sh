f3cb79f320de6accd35fe23a5671ffc173d4c56a
Anthony Davis <adavis619@gmail.com>
2025-04-18 08:15:54 -0700

    Update dbi_grepper.sh

--- dbi_grepper.sh
+++ dbi_grepper.sh
@@ -9,7 +9,7 @@
 4. Date: 04/17/2025
 5. Usage: dbi_grepper.sh <file1> <file2>
 '
-
+items_dir="$CCRUN/conf/webess/flowsheets/items"
 # Define color codes
 YELLOW='\e[33m'
 GREEN='\e[32m'
@@ -24,7 +24,18 @@ if [[ -z "$inputfile" || ! -f "$inputfile" ]]; then
 fi
 
 # echo the filename, get non-commented lines, isolate dbis, replace leading/trailing whitespace with quotes and pipes, then output to one line. Final sed to add new line for loops.
-output=$(for file in "$@"; do echo -e "${YELLOW}$file${NC}"; grep '^[^#]' $CCRUN/conf/webess/flowsheets/items/"$file" | awk -F'[' '{print $1}' | sed 's/^[[:space:]]*/"/;s/[[:space:]]*$/"\|/' | tr -d '\n' | sed $'$s/$/\\\n/'; done)
+output=$(for file in "$@"; do 
+echo -e "${YELLOW}$file${NC}"
+path="$items_dir/$file"
+
+if [[ ! -r "$path" ]]; then
+  echo "$path not found or unreadable">&2
+  continue
+fi
+
+echo "grepping: $path" >&2
+
+grep '^[^#]' "$path" | awk -F'[' '{print $1}' | sed 's/^[[:space:]]*/"/;s/[[:space:]]*$/"\|/' | tr -d '\n' | sed $'$s/$/\\\n/'; done)
 #output=$(for file in "$@"; do echo -e "${YELLOW}$file${NC}"; grep '^[^#]' "$file" | awk -F'[' '{print $1}' | sed 's/^[[:space:]]*/"/;s/[[:space:]]*$/"\|/' | tr -d '\n' | sed $'$s/$/\\\n/'; echo; done)
 if [ -n "$output" ]; then echo -n "$output"; fi
 
