c60a1438f3b9d4369de32b9c8faaf4980531970c
Anthony Davis <adavis619@gmail.com>
2025-04-21 10:01:30 -0700

    Update dbi_grepper.sh

--- dbi_grepper.sh
+++ dbi_grepper.sh
@@ -7,38 +7,43 @@
 2. Description: dbi_grepper.sh
 3. Author: Anthony Davis
 4. Date: 04/17/2025
-5. Usage: dbi_grepper.sh <file1> <file2>
+5. Usage: asg_ez_dbigrep.sh <file1> <file2>
 '
+
+# set items directory path
 items_dir="$CCRUN/conf/webess/flowsheets/items"
+
 # Define color codes
 YELLOW='\e[33m'
 GREEN='\e[32m'
 RED='\e[31m'
 NC='\e[0m'
 
+# check for argument
 inputfile="$1"
 
-if [[ -z "$inputfile" || ! -f "$inputfile" ]]; then
+if [ -z "$inputfile" ]; then
   echo "Usage: $0 <file1> <file2>"
   exit 1
 fi
 
-# echo the filename, get non-commented lines, isolate dbis, replace leading/trailing whitespace with quotes and pipes, then output to one line. Final sed to add new line for loops.
-output=$(for file in "$@"; do 
-echo -e "${YELLOW}$file${NC}"
-path="$items_dir/$file"
+# echo the filename to user
+output=$(for file in "$@"; do
+             echo -e "${YELLOW}$file${NC}"
+             echo -e -n "${GREEN}grep -E '(${NC}"
+             # then confirm items path
+             path="$items_dir/$file"
 
-if [[ ! -r "$path" ]]; then
-  echo "$path not found or unreadable">&2
-  continue
-fi
+             if [[ ! -r "$path" ]]; then
+                 echo "$path not found">&2
+                 continue
+             fi
 
-echo "grepping: $path" >&2
-
-grep '^[^#]' "$path" | awk -F'[' '{print $1}' | sed 's/^[[:space:]]*/"/;s/[[:space:]]*$/"\|/' | tr -d '\n' | sed $'$s/$/\\\n/'; done)
-#output=$(for file in "$@"; do echo -e "${YELLOW}$file${NC}"; grep '^[^#]' "$file" | awk -F'[' '{print $1}' | sed 's/^[[:space:]]*/"/;s/[[:space:]]*$/"\|/' | tr -d '\n' | sed $'$s/$/\\\n/'; echo; done)
-if [ -n "$output" ]; then echo -n "$output"; fi
+# get non-commented lines, isolate dbis, replace leading/trailing whitespace with quotes and pipes, then output to one line. Final sed to add new line for loops.
+grep '^[^#]' "$path" | awk -F'[' '{print $1}' | sed 's/^[[:space:]]*/"/;s/[[:space:]]*$/"\|/' | tr -d '\n' | sed $'$s/$/\\\n/' | sed -r 's/(.*)\|/\1/'; done)
+if [ -n "$output" ]; then echo -e -n "\n$output${GREEN})' site_dbshm.cf${NC}"; fi
 
 echo
 
 exit 0
+
