127c9d112510acf67a5b423faffb8472740d0602
Anthony Davis <adavis619@gmail.com>
2025-05-02 14:26:03 -0700

    Update asgr.sh

--- asgr.sh
+++ asgr.sh
@@ -3,85 +3,89 @@
 . /etc/.profile
 #set -xv
 
-: '
-1. Purpose: To rsync file/directory to redundant servers faster than R-copy
-2. Description: run "asgr <filename>" for "asgr <directory name>.  This script is available in $LBIN in all DoD servers.
-                This is owner specific.  Run as ".dosu asgr .." otherwise the file/directory will be owned by you in redundant servers
-3. Author: 
-4. Date: 
-4. Usage: asgr <filename>
-'
-
-if [ $# == 0 ]; then
+# Usage helper
+tshow_usage() {
     echo ""
     echo "Usage:"
-    echo "...To push file or directory to all redundant servers..."
-    echo ".dosu asgr [FILE]"
-    echo ".dosu asgr [FULL PATH OF A DIRECTORY]"
-    echo "...To push file or directory to one specific redundant server..."
-    echo ".dosu asgr [FILE] server#"
-    echo ".dosu asgr [FULL PATH OF A DIRECTORY] server#"
+    echo "  .dosu asgr [-c] [-f] <file_or_dir> [server]"
+    echo "    -c    run CTImport (check-in) after rsync"
+    echo "    -f    treat the target as a full path instead of syncing from current directory"
+    echo "    server (optional) limits sync to that single host"
     echo ""
-    exit 1;
+    exit 1
+}
+
+if [ $# -eq 0 ]; then
+    show_usage
 fi
 
-while getopts c: option
-do
-    case "${option}"
-        in
-        c) if [ $# -gt 2 ]; then
-    FN=`echo $2 | sed  's/^\.\///'`
-    RSERVER=$3
-    .dosu rsync -azv $FN $RSERVER:`pwd`
-    .dosu CTImport -f $FN
-    exit 1;
+# Parse flags
+CHECKIN=false
+FULLPATH=false
+while getopts "cf" opt; do
+    case "$opt" in
+        c) CHECKIN=true ;;  # run CTImport after rsync
+        f) FULLPATH=true ;; # use full path logic
+        *) show_usage ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# Validate remaining args
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    show_usage
+fi
+
+TARGET="$1"
+
+# Determine rsync source and remote destination path
+if $FULLPATH; then
+    if [ -d "$TARGET" ]; then
+        # Directory: preserve trailing slash
+        DIRNAME="${TARGET%/}"
+        RSYNC_SRC="$DIRNAME/"
+        RSYNC_DEST="$DIRNAME/"
+    elif [ -f "$TARGET" ]; then
+        DIRNAME="$(dirname "$TARGET")"
+        BASEFILE="$(basename "$TARGET")"
+        RSYNC_SRC="$DIRNAME/$BASEFILE"
+        RSYNC_DEST="$DIRNAME/"
+    else
+        echo "ERROR: $TARGET is not a valid file or directory" >&2
+        exit 1
+    fi
 else
-    FN=`echo $2 | sed  's/^\.\///'`
-    RSERVER=`printf '%s\n' "${SYSLIST//$HOST/}"`
-    for rserv in `echo $RSERVER`
-    do
-        .dosu rsync -azv $FN $rserv:`pwd`
-        .dosu CTImport -f $FN
-    done
+    # Original behavior: sync from current working directory
+    FN="${TARGET#./}"
+    RSYNC_SRC="$FN"
+    RSYNC_DEST="$(pwd)/"
 fi
-esac
 
+# Build list of target servers
+if [ $# -eq 2 ]; then
+    SERVERS=("$2")
+else
+    IFS=$'\n' read -r -d '' -a SERVERS < <(printf '%s\n' "${SYSLIST//$HOST/}" && printf '\0')
+fi
 
-# For Rsync Dir, we can use this:
-for rserv in `echo $RSERVER`
-do
-.dosu rsync -azv $dirname/ $rserv:$dirname
+# Loop through each server
+for rserv in "${SERVERS[@]}"; do
+    # Auto-create remote directory
+    .dosu ssh -n "$rserv" "mkdir -p '$RSYNC_DEST'"
+    
+    # Perform rsync
+    .dosu rsync -azv -- "$RSYNC_SRC" "$rserv:$RSYNC_DEST" || {
+        echo "ERROR: rsync to $rserv failed" >&2
+        exit 1
+    }
+
+    # Run CTImport if requested
+    if $CHECKIN; then
+        .dosu CTImport -f -- "$RSYNC_SRC" || {
+            echo "ERROR: CTImport on $rserv failed" >&2
+            exit 1
+        }
+    fi
 done
 
-# for Rsync file with full path, we'll probably need dirname and basename utils
-e.g.,
-basedir=$(dirname `echo $1`)
-basefile=$(basename `echo $1`)
-exit
-
-done
-
-if [ $# -gt 1 ]; then
-    FN=`echo $1 | sed  's/^\.\///'`
-    RSERVER=$2
-    .dosu rsync -azv $FN $RSERVER:`pwd`
-    exit 1;
-else
-    FN=`echo $1 | sed  's/^\.\///'`
-    RSERVER=`printf '%s\n' "${SYSLIST//$HOST/}"`
-    for rserv in `echo $RSERVER`
-    do
-        .dosu rsync -azv $FN $rserv:`pwd`
-    done
-fi
-
-exit
-
-asgr was written based on assumption that the file will be remotely sync'd from pwd, as "R" script does.
-
-I would like to retain what asgr is doing now but add a flag "-f" so that it takes full path dir/file names
-
-e.g.,
-
-.dosu asgr -f $CCRUN/conf/webess/cpoe2/categories
-.dosu asgr -f $CCRUN/conf/webess/screens
+exit 0
