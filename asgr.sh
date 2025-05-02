17f640239d0f27650691b36d82e0207a1fcf47db
Anthony Davis <adavis619@gmail.com>
2025-05-02 14:06:54 -0700

    Create asgr.sh

---
+++ asgr.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+. /etc/.profile
+#set -xv
+
+: '
+1. Purpose: To rsync file/directory to redundant servers faster than R-copy
+2. Description: run "asgr <filename>" for "asgr <directory name>.  This script is available in $LBIN in all DoD servers.
+                This is owner specific.  Run as ".dosu asgr .." otherwise the file/directory will be owned by you in redundant servers
+3. Author: 
+4. Date: 
+4. Usage: asgr <filename>
+'
+
+if [ $# == 0 ]; then
+    echo ""
+    echo "Usage:"
+    echo "...To push file or directory to all redundant servers..."
+    echo ".dosu asgr [FILE]"
+    echo ".dosu asgr [FULL PATH OF A DIRECTORY]"
+    echo "...To push file or directory to one specific redundant server..."
+    echo ".dosu asgr [FILE] server#"
+    echo ".dosu asgr [FULL PATH OF A DIRECTORY] server#"
+    echo ""
+    exit 1;
+fi
+
+while getopts c: option
+do
+    case "${option}"
+        in
+        c) if [ $# -gt 2 ]; then
+    FN=`echo $2 | sed  's/^\.\///'`
+    RSERVER=$3
+    .dosu rsync -azv $FN $RSERVER:`pwd`
+    .dosu CTImport -f $FN
+    exit 1;
+else
+    FN=`echo $2 | sed  's/^\.\///'`
+    RSERVER=`printf '%s\n' "${SYSLIST//$HOST/}"`
+    for rserv in `echo $RSERVER`
+    do
+        .dosu rsync -azv $FN $rserv:`pwd`
+        .dosu CTImport -f $FN
+    done
+fi
+esac
+exit
+
+done
+
+if [ $# -gt 1 ]; then
+    FN=`echo $1 | sed  's/^\.\///'`
+    RSERVER=$2
+    .dosu rsync -azv $FN $RSERVER:`pwd`
+    exit 1;
+else
+    FN=`echo $1 | sed  's/^\.\///'`
+    RSERVER=`printf '%s\n' "${SYSLIST//$HOST/}"`
+    for rserv in `echo $RSERVER`
+    do
+        .dosu rsync -azv $FN $rserv:`pwd`
+    done
+fi
+
+exit
