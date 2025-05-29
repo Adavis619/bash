cc95c9d7ccf7b59a1cdbde72e500d5d44acd05c8
Anthony Davis <adavis619@gmail.com>
2025-05-29 06:29:27 -0700

    Create asg_dasdCheck.sh

---
+++ asg_dasdCheck.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+. /etc/.profile
+#set -xv
+
+: '
+1. Purpose: Check to see status of START_DASD in CHKDAEMONS.$HOST.cf file. Alert sent if dasd is running on host, but START_DASD is commented out.
+2. Description: asg_dasdCheck.sh
+3. Author: Anthony Davis
+4. Date: 04/10/2025
+5. Usage: asg_dasdCheck.sh run via cis cron job.
+'
+
+
+echo `date`" Start asg_dasdCheck.sh"
+
+for i in `cat /$CCSYSDIR/EHR/serverlist/serverlist_monitor_VA`; do
+alerts=$(
+dasdchk=$(ssh -n $i 'cd $CCSYSDIR; P dasd | grep -w dasd | grep $CAMPUS >/dev/null && grep -w "^#.*START_DASD" CHKDAEMONS.$HOST.cf');
+if echo "$dasdchk" | grep -q START_DASD; then echo -e "Check the following configuration in file: \$CCSYSDIR/CHKDAEMONS.\$HOST.cf:\n $dasdchk"; fi)
+
+#if [ -n "$alerts" ]; then echo "$alerts" | mailx -s "[ASG][asg_dasdCheck.sh][OG]: $i: START_DASD commented out in CHKDAEMONS.\$HOST.cf" anthony.davis@clinicomp.com; fi
+if [ -n "$alerts" ]; then echo "$alerts" | mailx -s "[ASG][asg_dasdCheck.sh][OG]: $i: START_DASD commented out in CHKDAEMONS.\$HOST.cf" asgp2@clinicomp.opsgenie.net; fi
+
+done
+
+echo `date`" End asg_dasdCheck.sh"
